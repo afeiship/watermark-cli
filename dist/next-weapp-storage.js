48a138ea50ed31f6251ad0ecceb8471510ce6133
afeiship <1290657123@qq.com>
2017-08-22 16:24:32 +0800

    issue: initail

---
+++ dist/next-weapp-storage.js
@@ -0,0 +1,27 @@
+(function () {
+
+  var global = global || window || self || this;
+  var nx = global.nx || require('next-js-core2');
+  var NxAbstractStorage = nx.AbstractStorage || require('next-abstract-storage');
+
+  var NxWeappStorage = nx.declare('nx.WeappStorage', {
+    extends: NxAbstractStorage,
+    methods:{
+      init: function(inPrefix){
+        this.base({
+          engine: wx,
+          prefix: inPrefix || '',
+          set: 'setStorageSync',
+          get: 'getStorageSync',
+          remove: 'removeStorageSync',
+          clear:'clearStorageSync'
+        });
+      }
+    }
+  });
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxWeappStorage;
+  }
+
+}());
